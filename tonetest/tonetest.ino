254beb5a3b1ea6003610677a38b6670cf2a62be8
erika.fung <erikaef978@gmail.com>
2020-04-11 20:08:49 -0600

    tone tutorial

--- tonetest/tonetest.ino
+++ tonetest/tonetest.ino
@@ -5,9 +5,13 @@ Adafruit Arduino - Lesson 10. Simple Sounds
 int speakerPin = 12;
  
 int numTones = 10;
-int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+//int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //            mid C  C#   D    D#   E    F    F#   G    G#   A
- 
+
+//https://www.arduino.cc/en/Tutorial/toneMelody
+int jawsTones[] = {82, 87};
+int noteDurations[] = {2, 1};
+
 void setup()
 {
  
